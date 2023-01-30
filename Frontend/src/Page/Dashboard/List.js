75de472aa89416357ba576307c13f40dbfe03355
Youssef Farahat <74507015+youssefkhaledfarahat@users.noreply.github.com>
2023-01-30 20:52:59 +0200

    Add files via upload

---
+++ Frontend/src/Page/Dashboard/List.js
@@ -0,0 +1,56 @@
+import React from 'react'
+
+
+function List({ employees, handleEdit, handleDelete }) {
+    return (
+        <div className='contain-table'>
+            <table className='striped-table'>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Job Title</th>
+                        <th>Department</th>
+                        <th colSpan={10} className="text-center">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {employees.length > 0 ? (
+                        employees.map((employee, i) => (
+                            <tr key={employee.id}>
+                                <td> {i + 1} </td>
+                                <td>{employee.firstName}</td>
+                                <td>{employee.lastName}</td>
+                                <td>{employee.jobTitle}</td>
+                                <td>{employee.department}</td>
+                                <td className="text-right">
+                                    <button
+                                        onClick={() => handleEdit(employee.id)}
+                                        className="button muted-button"
+                                    >
+                                        Edit
+                                    </button>
+                                </td>
+                                <td className="text-left">
+                                    <button
+                                        onClick={() => handleDelete(employee.id)}
+                                        className="button muted-button"
+                                    >
+                                        Delete
+                                    </button>
+                                </td>
+                            </tr>
+                        ))
+                    ) : (
+                        <tr>
+                            <td colSpan={10}>No Employees</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default List
